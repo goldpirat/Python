3e51bc6bcb935bc6b6be5a42e79642950af03d45
Flori Kusari <149197022+goldpirat@users.noreply.github.com>
2025-02-04 16:49:47 +0100

    Text Justification
    Given an array of words and a maxWidth, format the text so that each line has exactly maxWidth characters and is fully (left and right) justified.

---
+++ Leetcode4FUn/TextJustification.py
@@ -0,0 +1,14 @@
+def fullJustify(words, maxWidth):
+    res, cur, num_of_letters = [], [], 0
+    
+    for word in words:
+        if num_of_letters + len(word) + len(cur) > maxWidth:
+            for i in range(maxWidth - num_of_letters):
+                cur[i % (len(cur) - 1 or 1)] += ' '
+            res.append("".join(cur))
+            cur, num_of_letters = [], 0
+        cur.append(word)
+        num_of_letters += len(word)
+    
+    res.append(" ".join(cur).ljust(maxWidth))
+    return res
